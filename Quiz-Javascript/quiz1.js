85b0fa2113556aeee7aef777b0467de5cb69b4dc
Kilian <31303@ma-web.nl>
2021-01-22 12:08:56 +0100

    Nieuwe opdrachten gemaakt

---
+++ Quiz-Javascript/quiz1.js
@@ -0,0 +1,78 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Metal Lisse Art Museum Quizz",
+        "imageURI": "heavymetal.png"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Welke metal genre is met Fantasy",
+				"answers": [{
+						"answer": "Power-Metal",
+						"feedback": true
+					},
+					{
+						"answer": "Industrial Metal",
+						"feedback": false
+					},
+					{
+						"answer": "Groove-Metal",
+						"feedback": false
+					},
+					{
+						"answer": "Thrash-Metal",
+						"feedback": false
+					},
+					{
+						"answer": "Heavy-Metal",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Welke Metal Genre is de band SLAYER!!",
+				"answers": [{
+						"answer": "Death-Metal",
+						"feedback": false
+					},
+					{
+						"answer": "Heavy-Metal",
+						"feedback": false
+					},
+					{
+						"answer": "Thrash-Metal",
+						"feedback": true
+					},
+					{
+						"answer": "Doom-Metal",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Welke Metal band heeft Heavy Metal in de wereld gebracht?",
+				"answers": [{
+						"answer": "Iron Maiden",
+						"feedback": false
+					},
+					{
+						"answer": "Led Zeppelin",
+						"feedback": false
+					},
+					{
+						"answer": "Black Sabbath",
+						"feedback": true
+					},
+					{
+						"answer": "Deff Leppard",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
+
